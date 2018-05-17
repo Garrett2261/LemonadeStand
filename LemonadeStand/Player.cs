c9022caa0a85147c7fbf4c08c67ee768874d9bce
Garrett <davisgarrett21@yahoo.com>
2018-05-16 20:15:05 -0500

    Added data to the player class that included the recipe for the Lemonade which is NumberOfLemons, CupsOfSugar, and NumberOfIceCubes

--- LemonadeStand/Player.cs
+++ LemonadeStand/Player.cs
@@ -9,9 +9,16 @@ namespace LemonadeStand
     public class Player
     {
         public int NumberOfDays;
-        public Player (int NumberOfDays)
+        public int NumberOfLemons;
+        public int CupsOfSugar;
+        public int NumberOfIceCubes;
+        public Player (int NumberOfDays, int NumberOfLemons, int CupsOfSugar, int NumberOfIceCubes)
         {
             this.NumberOfDays = NumberOfDays;
+            this.NumberOfLemons = NumberOfLemons;
+            this.CupsOfSugar = CupsOfSugar;
+            this.NumberOfIceCubes = NumberOfIceCubes;
+
 
         }
         
