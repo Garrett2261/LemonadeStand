e3ead0dda9d0927a36e4f4f4b3a7da9ed296759a
Garrett <davisgarrett21@yahoo.com>
2018-05-16 19:56:19 -0500

    Initial Commit for repository

---
+++ LemonadeStand/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class Player
+    {
+        public int NumberOfDays;
+        public Player (int NumberOfDays)
+        {
+            this.NumberOfDays = NumberOfDays;
+
+        }
+        
+    }
+}
