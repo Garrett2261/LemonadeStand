0f566253198a0896d0309b7bb089170323e1ef1f
Garrett <davisgarrett21@yahoo.com>
2019-04-18 19:23:05 -0500

    Added two more functions to the recipe class. Added a get number of cups of sugar used that would compare the cups of sugar wanted for the recipe versus the amount in inventory and if the amount wanted was less than or equal to the number they had in the inventory, then they could do that. If the number they wanted to use was more than what they had in their inventory, then that had to either buy some more sugar from the store or use a lower number. The second function was a function that got the number of ice cubes for the recipe and it had the same function as the previous one.

--- LemonadeStand/Recipe.cs
+++ LemonadeStand/Recipe.cs
@@ -19,7 +19,7 @@ namespace LemonadeStand
 
         public int GetNumberOfLemonsUsed()
         {
-            Console.WriteLine("The basic recipe amounts for 4 lemons, 4 cups of sugar, and 4 ice cubes. You can change this and set it to whatever you'd like.");
+            Console.WriteLine("The basic recipe amounts for 4 lemons. You can change this and set it to whatever you'd like.");
             Console.WriteLine("How many lemons would you like to use in your recipe?");
             string number = Console.ReadLine();
             int amountOfLemons = Int32.Parse(number);
@@ -33,6 +33,40 @@ namespace LemonadeStand
             }
             return lemons;
 
+        }
+
+        public int GetNumberOfCupsOfSugarUsed()
+        {
+            Console.WriteLine("The basic recipe amounts for 4 cups of sugar. You can change this and set it to whatever you'd like.");
+            Console.WriteLine("How many cups of sugar would you like to use in your recipe?");
+            string number = Console.ReadLine();
+            int sugar = Int32.Parse(number);
+            if(sugar <= Inventory.cupsOfSugar)
+            {
+                cupsOfSugar = sugar;
+            }
+            else if(sugar > Inventory.cupsOfSugar)
+            {
+                Console.WriteLine("You don't have that many cups of sugar. Either buy some more from the store, or choose a lower amount.");
+            }
+            return cupsOfSugar;
+        }
+
+        public int GetNumberOfIceCubesUsed()
+        {
+            Console.WriteLine("The basic recipe amounts for 4 ice cubes. You can change this and set it to whatever you'd like.");
+            Console.WriteLine("How many ice cubes would you like to use in your recipe?");
+            string number = Console.ReadLine();
+            int ice = Int32.Parse(number);
+            if(ice <= Inventory.iceCubes)
+            {
+                iceCubes = ice;
+            }
+            else if(ice > Inventory.iceCubes)
+            {
+                Console.WriteLine("You don't have that many ice cubes. Either buy some more from the store, or choose a lower amount.");
+            }
+            return iceCubes;
         }
     }
 }
